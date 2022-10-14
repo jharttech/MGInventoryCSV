be538d8d0caecff9a942dff3e18cc11c5da6fe24
Jason Hartgraves <jharttech@hotmail.com>
2022-10-13 20:43:44 -0500

    Rewrote code to move more towards OOP.

--- MakeCSV.py
+++ MakeCSV.py
@@ -1,13 +1,26 @@
 import sys
 import csv
 import re
+import subprocess
+import glob
 
 
 class Setup:
-    def __init__(self, cart, po, unit_count, year, sem_code, unit_type, start_num, end_num):
-        self.info = (cart, po, unit_count, year, sem_code, unit_type, start_num, end_num)
-        return self.info
+    def __init__(
+        self, cart, po, unit_count, year, sem_code, unit_type, start_num, end_num
+    ):
+        self.info = (
+            cart,
+            po,
+            unit_count,
+            year,
+            sem_code,
+            unit_type,
+            start_num,
+            end_num,
+        )
 
+        return self.info
 
     @classmethod
     def get(cls):
@@ -27,13 +40,15 @@ class Setup:
             else:
                 break
         while True:
-            sem_code = input("Please enter the semester code (FA, SP, SU): ")
+            sem_code = input("Please enter the semester code (FA, SP, SU): ").upper()
             if not re.search(r"^(FA|SP|SU)$", sem_code):
                 print(f"Invalid Semester Code")
             else:
                 break
         while True:
-            unit_type = input("Please enter the type of units (CB, P, PJ, HF): ")
+            unit_type = input(
+                "Please enter the type of units (CB, P, PJ, HF): "
+            ).upper()
             if not re.search(r"^(CB|PJ|P|HF)$", unit_type):
                 print("Invalid Unit Type")
             else:
@@ -43,17 +58,18 @@ class Setup:
             if not re.search(r"^[0-9]{4}$", start_num):
                 print("Invalid start number, ex. 2234")
             else:
-                validate = input(f"You have entered {start_num} as starting serial number, is that correct? y/n ")
+                validate = input(
+                    f"You have entered {start_num} as starting serial number, is that correct? y/n "
+                ).lower()
                 if re.search(r"^(y|n)$", validate, flags=re.IGNORECASE):
                     start_num = int(start_num)
                     break
-        end_num = ((unit_count - 1) + start_num)
+        end_num = (unit_count - 1) + start_num
 
-        return cart,po,unit_count,year,sem_code,unit_type,start_num,end_num
+        return cart, po, unit_count, year, sem_code, unit_type, start_num, end_num
 
 
-
-class Compose():
+class Compose:
     def __init__(self, info):
         self.new_file(info)
         self.write_file(info)
@@ -67,20 +83,43 @@ class Compose():
 
     def write_file(self, info):
         for i in range(0, int(info[2])):
-            self.current_num = (int(info[6]) + i)
-            self.asset_tag = (info[3]+info[4]+str(self.current_num)+info[5],"","","")
-            with open(self.filename, mode='a') as csv_file:
-                unit_file = csv.writer(csv_file, delimiter=',')
+            self.current_num = int(info[6]) + i
+            self.asset_tag = (
+                info[3] + info[4] + str(self.current_num) + info[5],
+                "",
+                "",
+                "",
+            )
+            with open(self.filename, mode="a") as csv_file:
+                unit_file = csv.writer(csv_file, delimiter=",")
                 unit_file.writerow(self.asset_tag)
 
 
+class Duplicate:
+    def __init__(self, info):
+        self.check(info)
+
+    def check(self, info):
+        self.test = []
+        self.files = glob.glob("*.csv")
+        for i in range(0, len(self.files)):
+            self.repeat_check = subprocess.Popen(
+                ["grep", str(info[6]), self.files[i]], stdout=subprocess.PIPE
+            )
+            self.search = self.repeat_check.stdout.read().decode().strip()
+            self.search = str(self.search)
+            print(f"\n{self.search} Found!! ---------> {self.files[i]}\n")
+            if re.search(r"^(.*)" + re.escape(str(info[6])) + "(.*)$", self.search):
+                raise ValueError("\n\tYou have a duplicate serial number!!\n")
+
+        return False
 
 
 def main():
     setup = Setup.get()
+    Duplicate(setup)
     compose = Compose(setup)
     print(compose)
-
 
 
 if __name__ == "__main__":
