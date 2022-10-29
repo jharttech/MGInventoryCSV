3d2a84a52f9c3b8413b1adefb8c49ac2af42de0c
Jason Hartgraves <jharttech@hotmail.com>
2022-10-28 20:43:25 -0500

    Upload of new code for MakeCSV, and updated README.md

--- MakeCSV.py
+++ MakeCSV.py
@@ -26,38 +26,62 @@ class Setup:
     def get(cls):
         cart = input("Please enter the cart name: ")
         po = input("Please enter the PO Number for the order: ")
+
+        # Using while loops to verify integrity of the input, if input is invalid,
+        # program will ask for input again until valid
         while True:
             unit_count = input("Please enter the number of units for the order: ")
+            # Use regex to verify integrity of input
             if not re.search(r"^[0-9]+$", unit_count):
                 print(f"Invalid Unit count")
             else:
+                # If the input is valid, convert it to an int
                 unit_count = int(unit_count)
                 break
+
+        # Using while loops to verify integrity of the input, if input is invalid,
+        # program will ask for input again until valid
         while True:
             year = input("Please enter the inventory year (ex. 22): ")
+            # Use regex to verify integrity of input
             if not re.search(r"^(?:2[2-9])$", year):
                 print(f"Invalid year")
             else:
                 break
+
+        # Using while loops to verify integrity of the input, if input is invalid,
+        # program will ask for input again until valid
         while True:
             sem_code = input("Please enter the semester code (FA, SP, SU): ").upper()
+            # Use regex to verify integrity of input
             if not re.search(r"^(FA|SP|SU)$", sem_code):
                 print(f"Invalid Semester Code")
             else:
                 break
+
+        # Using while loops to verify integrity of the input, if input is invalid,
+        # program will ask for input again until valid
         while True:
             unit_type = input(
                 "Please enter the type of units (CB, P, PJ, HF): "
             ).upper()
+            # Use regex to verify integrity of input
             if not re.search(r"^(CB|PJ|P|HF)$", unit_type):
                 print("Invalid Unit Type")
             else:
                 break
+
+        # Using while loops to verify integrity of the input, if input is invalid,
+        # program will ask for input again until valid
         while True:
             start_num = input("Please enter the starting serial number: ")
-            if not re.search(r"^[0-9]{4}$", start_num):
+            # Use regex to verify integrity of input
+            if not re.search(
+                r"^[0-9]{4}$", start_num
+            ):  # Expand this to {5} if serial is above 9999
                 print("Invalid start number, ex. 2234")
             else:
+
                 validate = input(
                     f"You have entered {start_num} as starting serial number, is that correct? y/n "
                 ).lower()
@@ -69,57 +93,44 @@ class Setup:
         return cart, po, unit_count, year, sem_code, unit_type, start_num, end_num
 
 
-class Compose:
-    def __init__(self, info):
-        self.new_file(info)
-        self.write_file(info)
+def compose(info):
+    filename = f"{info[0]}-PO{info[1]}-Units{info[2]}-{info[6]}-{info[7]}.csv"
+    for i in range(0, int(info[2])):
+        current_num = int(info[6]) + i
+        asset_tag = (
+            info[3] + info[4] + str(current_num) + info[5],
+            "",
+            "",
+            "",
+        )
+        with open(filename, mode="a") as csv_file:
+            unit_file = csv.writer(csv_file, delimiter=",")
+            unit_file.writerow(asset_tag)
+    return f"New file {filename} has been composed."
 
-    def __str__(self):
-        return f"New file {self.filename} has been composed."
 
-    def new_file(self, info):
-        self.filename = f"{info[0]}-PO{info[1]}-Units{info[2]}-{info[6]}-{info[7]}.csv"
-        return self.filename
-
-    def write_file(self, info):
-        for i in range(0, int(info[2])):
-            self.current_num = int(info[6]) + i
-            self.asset_tag = (
-                info[3] + info[4] + str(self.current_num) + info[5],
-                "",
-                "",
-                "",
+def duplicate(info):
+    files = glob.glob("*.csv")
+    for i in range(0, len(files)):
+        repeat_check = subprocess.Popen(
+            ["grep", str(info[6]), files[i]], stdout=subprocess.PIPE
+        )
+        search = str(repeat_check.stdout.read().decode().strip())
+        if re.search(r"^(.*)" + re.escape(str(info[6])) + "(.*)$", search):
+            raise ValueError(
+                f"\nYou have a duplicate serial number!!: \n\n{search} --> file: {files[i]}\n"
             )
-            with open(self.filename, mode="a") as csv_file:
-                unit_file = csv.writer(csv_file, delimiter=",")
-                unit_file.writerow(self.asset_tag)
-
-
-class Duplicate:
-    def __init__(self, info):
-        self.check(info)
-
-    def check(self, info):
-        self.test = []
-        self.files = glob.glob("*.csv")
-        for i in range(0, len(self.files)):
-            self.repeat_check = subprocess.Popen(
-                ["grep", str(info[6]), self.files[i]], stdout=subprocess.PIPE
-            )
-            self.search = self.repeat_check.stdout.read().decode().strip()
-            self.search = str(self.search)
-            print(f"\n{self.search} Found!! ---------> {self.files[i]}\n")
-            if re.search(r"^(.*)" + re.escape(str(info[6])) + "(.*)$", self.search):
-                raise ValueError("\n\tYou have a duplicate serial number!!\n")
-
-        return False
+    return False
 
 
 def main():
-    setup = Setup.get()
-    Duplicate(setup)
-    compose = Compose(setup)
-    print(compose)
+    if len(sys.argv) > 1:
+        sys.exit("Too many command arguments.  Usage: python3 project.py")
+    else:
+        setup = Setup.get()
+        check = duplicate(setup)
+        if check == False:
+            compose(setup)
 
 
 if __name__ == "__main__":
--- README.md
+++ README.md
@@ -1,2 +1,28 @@
-# MGInventoryCSV
-Placeholder for simple script to help create needed inventory CSV files for Mountain Grove R3
+<h1 align="center"><b>CS50P Project</b></h1>
+<h2>Title: MGInventoryCSV</b></h2>
+<h2><b>Overview</b></h2>
+<p>This program was designed to meet a specific need of creating a Comma Separated Value file for inventory purposes on the Mountain Grove Campus.  These csv files are sent to a vendor when purchasing chromebooks, so that the vendor can U.V. print our custom campus inventory I.D. from the csv to each chromebook.  They then populate the rest of the csv file with the Manufacture Serial Number, the vendor Warranty Serial Number, and the unit MAC Address.  They then send it back to me for my inventory system.</p>
+<p>I originally created a class for each of the functions, but decided that a class was only really necessary for the 'info' data structure and the rest would be better as functions.</p>
+<p>I struggled with deciding on prompting the user to re-enter data if the user input did not pass the regex check or to stop the program on invalid data entry. If the program did not re-prompt the user and instead stopped the program, the code would be cleaner than having multiple 'while' loops, but the user experience would suffer if the user had to re-enter all the data every time there was a typo.</p>
+<p>I did not add a header row to the csv files as the vendor that uses this data often restructures the order of the data with their own headers.  It would be easy to implement using a dictionary and the writeheader() function included in the <a href="https://docs.python.org/3/library/csv.html#module-csv">csv</a> module, or by manually having the csv writer create the first row with header information.</p></br>
+<h4><b>Needed Modules</b></h4>
+<h5><b>Note: These are all included in <a href="https://docs.python.org/3/library/index.html">The Python Standard Library</a> version 3.10.8</b></h5>
+<ul><a href="https://docs.python.org/3/library/sys.html">sys</a></br>
+<a href="https://docs.python.org/3/library/csv.html#module-csv">csv</a></br>
+<a href="https://docs.python.org/3/library/re.html#regular-expression-syntax">re</a></br>
+<a href="https://docs.python.org/3/library/subprocess.html">subprocess</a></br>
+<a href="https://docs.python.org/3/library/glob.html">glob</a></br></ul></br>
+<h4><b>Included test_project.py File</b></h4>
+<p>The test_project.py file included in the project is just a basic test file that tests the Class methods and the program functions using the <a href="https://docs.pytest.org/en/7.1.x/">pytest</a> module.
+<h4><b>Setup()</b></h4>
+<p>The Setup class takes user input to declare the cart the units belong too, the Purchase Order number for the units, the year the units were purchases, the semester code during which the units were purchased, the type of units (predefined codes for our campus), and the starting unit serial number.  The ending unit serial number is calculated by the class, based on the number of units in specified for the order.</p>
+The class uses regex checking from the <a href="https://docs.python.org/3/library/re.html#regular-expression-syntax">re</a> module to verify that the user is inputting data correctly, and will re-prompt the user to input correct data for the requested data.</p></br>
+<h4><b>duplicate()</b></h4>
+<p>The duplicate function is designed to check for the use of duplicate serial numbers.  This would cause an issue as we only want unique serial numbers.</p>
+<p>The function uses the <a href="https://docs.python.org/3/library/glob.html">glob</a> module to create a list of existing csv files in the current directory.  The function then "greps", using the <a href="https://docs.python.org/3/library/subprocess.html">subprocess</a> module, through each file and checks to see if there are any occurrences of the starting unit serial number using a regex.</p>
+<p>If an occurrence is found then a ValueError is raised telling the user which csv file holds the occurrence.  This is to make sure that the user does not inadvertently reuse serial numbers.</p>
+<p>If no duplicate is found then the function returns "False".</p></br>
+<h4><b>compose()</b></h4>
+<p>The compose function is designed to compose the csv file with the correct file name of</p>
+<p>("cart name"-"PO Number"-"number of units"-"start serial number"-"end serial number".csv)</p>
+<p>The function creates the file if it does not exist, then appends each line (unit info) to the file.</p>
